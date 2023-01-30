39d825abe8a9420e6b65b9296bd9bd22e41e4e29
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-01-30 12:08:53 -0500

    update

---
+++ restaurants.js
@@ -0,0 +1,98 @@
+export const restaurants = [
+  {
+    name: "Punto Cubano",
+    dishes: [
+      {
+        name: "Chicken Sandwich",
+        price: 20000,
+        description: "Tomato, Lettuce, House Sauce and Chicken",
+      },
+      {
+        name: "Pork Sandwich",
+        price: 18000,
+        description: "Tomato, Lettuce, House Sauce and Pork",
+      },
+      {
+        name: "Beef Sandwich",
+        price: 20000,
+        description: "Tomato, Lettuce, House Sauce and Beef",
+      },
+      {
+        name: "Cordero Sandwich",
+        price: 15000,
+        description: "Tomato, Lettuce, House Sauce and Cordero",
+      },
+      {
+        name: "Ropa Vieja Sandwich",
+        price: 20000,
+        description: "Tomato, Lettuce, House Sauce and shredded Meat",
+      },
+    ],
+    image: require("./assets/restaurants/QBano.png"),
+  },
+  {
+    name: "Wings and Beer",
+    dishes: [
+      {
+        name: "6 Wings",
+        price: 24900,
+        description: "6 Wings with Criolla Potato",
+      },
+      {
+        name: "12 Wings",
+        price: 42900,
+        description: "12 Wings with Criolla Potato",
+      },
+      {
+        name: "24 Wings",
+        price: 66900,
+        description: "24 Wings with Criolla Potato",
+      },
+      {
+        name: "50 Wings",
+        price: 129900,
+        description: "50 Wings with Criolla Potato",
+      },
+      {
+        name: "Nachos Tex-Mex",
+        price: 32900,
+        description:
+          "Chicken, Beef, shredded Pork, Cheese, Buffalo sauce, chilli, Guacamole and Sour Cream ",
+      },
+    ],
+    image: require("./assets/restaurants/mrwings.jpeg"),
+  },
+  {
+    name: "KFC",
+    dishes: [
+      {
+        name: "Kentucy Sandwich",
+        price: 1600,
+        description: "The famous sandwich with Sundae and Coke",
+      },
+      {
+        name: "Wow Box BBQ Pop",
+        price: 20900,
+        description: "Sandwich, Fries, Pop corn and Coke",
+      },
+      {
+        name: "Duo Kectucky Deluxe",
+        price: 59500,
+        description:
+          "2 Sandwiches, 2 fries, 1 Pop corn, 1 Avalancha Oreo and 2 Cokes",
+      },
+      {
+        name: "Mega Familiar",
+        price: 72900,
+        description: "10 Pieces of Chicken and 4 fries",
+      },
+      {
+        name: "Mega Recargado XXL",
+        price: 89900,
+        description:
+          "8 Pieces of Chicken, 4 Strips, 4 Wings, 2 salads, 1 fries and 1 Big Pop corn",
+      },
+    ],
+    image: require("./assets/restaurants/kfc.png"),
+  },
+];
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ScrollView, TouchableOpacity } from "react-native";
 import React from "react";
+import { restaurants } from "../restaurants";
 
 const HomeScreen = () => {
+  const renderRestaurants = restaurants.map((restaurant) => (
+    <TouchableOpacity>
+      <Image
+        source={restaurant.image}
+        className="h-60 w-60 rounded-lg"
+        style={{ resizeMode: "stretch" }}
+      />
+    </TouchableOpacity>
+  ));
   return (
-    <View>
-      <Text>Home</Text>
+    <View className="mt-12 items-center">
+      <ScrollView className="space-y-6">{renderRestaurants}</ScrollView>
     </View>
   );
 };
