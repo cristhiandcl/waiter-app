259a5624cd635f7584588638745717de3c896467
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-01-31 16:46:32 -0500

    update

--- components/BasketItem.js
+++ components/BasketItem.js
@@ -1,20 +1,29 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { TrashIcon } from "react-native-heroicons/solid";
+import { removeFromBasket } from "../features/basketSlice";
+import { useDispatch } from "react-redux";
 
 const BasketItem = ({ item }) => {
+  const dispatch = useDispatch();
+
+  const removeItemsFromBasket = () => {
+    dispatch(removeFromBasket(item[0].id));
+  };
   return (
-    <View className="w-3/4 mx-auto items-center space-y-1">
-      <Text className="font-extrabold text-xl">{item[0].name}</Text>
-      <Text className="text-center font-semibold text-green-600">
-        {item[0].description}
-      </Text>
-      <Text className="text-red-600 font-extrabold">{item[0].price}</Text>
+    <View className="flex-row justify-center items-baseline">
+      <View className="w-3/4">
+        <Text className="font-extrabold text-xl">{item[0].name}</Text>
+        <Text className="font-semibold text-green-600">
+          {item[0].description}
+        </Text>
+        <Text className="text-red-600 font-extrabold">{item[0].price}</Text>
+      </View>
       <View className="flex-row items-center space-x-2">
         <Text>{item.length}</Text>
-        {/* <TouchableOpacity onPress={addItemsToBasket}> */}
-        <TrashIcon size={30} color="green" />
-        {/* </TouchableOpacity> */}
+        <TouchableOpacity onPress={removeItemsFromBasket}>
+          <TrashIcon size={30} color="green" />
+        </TouchableOpacity>
       </View>
     </View>
   );
