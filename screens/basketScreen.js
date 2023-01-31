8901e71e2be2cecd9d3b7775ea48285a2f81a684
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-01-31 14:58:38 -0500

    update

--- screens/basketScreen.js
+++ screens/basketScreen.js
@@ -1,18 +1,22 @@
 import { View, Text, SafeAreaView } from "react-native";
-import React, { useEffect } from "react";
+import React, { useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import { getBasketItems } from "../features/basketSlice";
 
 const BasketScreen = () => {
   const items = useSelector(getBasketItems);
+  const [groupedItemsInBasket, setGroupItemsInBasket] = useState();
 
-  useEffect(() => {
-    const duplicate = items?.reduce();
-    console.log(duplicate);
-    return () => {};
-  }, []);
+  useMemo(() => {
+    const groupItems = items.reduce((results, item) => {
+      (results[item.id] = results[item.id] || []).push(item);
+      return results;
+    }, {});
 
-  console.log(items);
+    setGroupItemsInBasket(groupItems);
+  }, [items]);
+
+  console.log(groupedItemsInBasket);
 
   return (
     <SafeAreaView className="">
