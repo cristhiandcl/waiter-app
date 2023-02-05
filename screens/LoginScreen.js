e68cae512a72846745227734778350d0c5ece07c
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-02-05 12:22:15 -0500

    update

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,6 +6,7 @@ import {
   Keyboard,
   Alert,
   TouchableWithoutFeedback,
+  KeyboardAvoidingView,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
@@ -44,49 +45,51 @@ const Login = () => {
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View
-        className="flex-1 items-center justify-center"
-        onPress={() => Keyboard.dismiss()}
-      >
-        <Text className="text-4xl text-center text-green-700 font-extrabold mb-2">
-          Welcome to the Orders Manager
-        </Text>
-        <Text className="w-3/4 text-center text-gray-400 font-extrabold text-xs mb-14 opacity-50">
-          Login if you already have an account or create one to get access
-        </Text>
-        <View className="w-2/3 space-y-6">
-          <View className="space-y-2">
-            <Text className="font-extrabold">E-mail</Text>
-            <TextInput
-              className="border-2 p-2"
-              value={userData.email}
-              onChangeText={(data) =>
-                onChangeText({ ...userData, email: data })
-              }
-              placeholder="Email"
-              autoCapitalize="none"
-            />
+      <KeyboardAvoidingView className="h-full" behavior="padding">
+        <View
+          className="flex-1 items-center justify-center"
+          onPress={() => Keyboard.dismiss()}
+        >
+          <Text className="text-4xl text-center text-green-700 font-extrabold mb-2">
+            Welcome to the Orders Manager
+          </Text>
+          <Text className="w-3/4 text-center text-gray-400 font-extrabold text-xs mb-14 opacity-50">
+            Login if you already have an account or create one to get access
+          </Text>
+          <View className="w-2/3 space-y-6">
+            <View className="space-y-2">
+              <Text className="font-extrabold">E-mail</Text>
+              <TextInput
+                className="border-2 p-2"
+                value={userData.email}
+                onChangeText={(data) =>
+                  onChangeText({ ...userData, email: data })
+                }
+                placeholder="Email"
+                autoCapitalize="none"
+              />
+            </View>
+            <View className="space-y-2">
+              <Text className="font-extrabold">Password</Text>
+              <TextInput
+                className="border-2 p-2"
+                value={userData.password}
+                onChangeText={(data) =>
+                  onChangeText({ ...userData, password: data })
+                }
+                placeholder="Password"
+                secureTextEntry={true}
+              />
+            </View>
+            <TouchableOpacity
+              className="bg-green-500 p-2 rounded"
+              onPress={logUser}
+            >
+              <Text className="text-center font-bold text-white">Login</Text>
+            </TouchableOpacity>
           </View>
-          <View className="space-y-2">
-            <Text className="font-extrabold">Password</Text>
-            <TextInput
-              className="border-2 p-2"
-              value={userData.password}
-              onChangeText={(data) =>
-                onChangeText({ ...userData, password: data })
-              }
-              placeholder="Password"
-              secureTextEntry={true}
-            />
-          </View>
-          <TouchableOpacity
-            className="bg-green-500 p-2 rounded"
-            onPress={logUser}
-          >
-            <Text className="text-center font-bold text-white">Login</Text>
-          </TouchableOpacity>
         </View>
-      </View>
+      </KeyboardAvoidingView>
     </TouchableWithoutFeedback>
   );
 };
