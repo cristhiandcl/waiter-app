d405af9bc996dc3a147bbe4ce9cbbbe10d1a7905
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-02-11 14:40:15 -0500

    update

--- components/SplitsRendering.js
+++ components/SplitsRendering.js
@@ -1,16 +1,43 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useMemo, useState } from "react";
 
 const SplitsRendering = ({ split, index }) => {
+  const [groupItemsInSplit, setGroupItemsInSplit] = useState();
+
+  useMemo(() => {
+    const itemsArranged = [...split];
+    let individualItems = [],
+      groupAllItems = [];
+    itemsArranged?.sort((r1, r2) =>
+      r1.id > r2.id ? 1 : r1.id < r2.id ? -1 : 0
+    );
+    itemsArranged?.map((_, index) => {
+      if (itemsArranged[index]?.id === itemsArranged[index + 1]?.id) {
+        individualItems.push(itemsArranged[index]);
+      } else {
+        individualItems.length === 0
+          ? individualItems.push(itemsArranged[index])
+          : individualItems.push(itemsArranged[index - 1]);
+        groupAllItems.push(individualItems);
+        individualItems = [];
+      }
+    });
+
+    setGroupItemsInSplit(groupAllItems);
+  }, []);
+
+  console.log("split", groupItemsInSplit);
+
   return (
     <View className="flex-row">
       <View className="flex-1 mb-2">
         <Text className="">Split {index + 1}</Text>
         <View className="my-1">
-          {split.map((spl) => (
-            <Text className="text-[10rem] font-extrabold flex-1">
-              {spl.name}
-            </Text>
+          {groupItemsInSplit.map((spl) => (
+            <View className="flex-row flex-1 space-x-1">
+              <Text className="text-[10rem] font-extrabold">{spl[0].name}</Text>
+              <Text className="text-[10rem] font-extrabold">x{spl.length}</Text>
+            </View>
           ))}
         </View>
       </View>
@@ -23,13 +50,13 @@ const SplitsRendering = ({ split, index }) => {
             .format(split.reduce((prev, item) => prev + item.price, 0))
             .replace(",00", "")}
         </Text>
-        {split.map((spl) => (
+        {groupItemsInSplit.map((spl) => (
           <Text className="text-[10rem] font-extrabold text-right">
             {new Intl.NumberFormat("es-CO", {
               style: "currency",
               currency: "COP",
             })
-              .format(spl.price)
+              .format(spl[0].price)
               .replace(",00", "")}
           </Text>
         ))}
