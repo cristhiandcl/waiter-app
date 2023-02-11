7b2126358818ec7670dc098af877fa1f7b2a8d36
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-02-11 14:13:48 -0500

    update

--- screens/basketScreen.js
+++ screens/basketScreen.js
@@ -83,18 +83,40 @@ const BasketScreen = () => {
   }, [items]);
 
   const renderSplits = splits?.map((split, index) => (
-    <View className="flex-row space-y-1">
-      <Text className="flex-1">Split {index + 1}</Text>
-      <Text>
-        {new Intl.NumberFormat("es-CO", {
-          style: "currency",
-          currency: "COP",
-        })
-          .format(split.reduce((prev, item) => prev + item.price, 0))
-          .replace(",00", "")}
-      </Text>
+    <View className="flex-row">
+      <View className="flex-1 mb-2">
+        <Text className="">Split {index + 1}</Text>
+        <View className="my-1">
+          {split.map((spl) => (
+            <Text className="text-[10rem] font-extrabold flex-1">
+              {spl.name}
+            </Text>
+          ))}
+        </View>
+      </View>
+      <View>
+        <Text>
+          {new Intl.NumberFormat("es-CO", {
+            style: "currency",
+            currency: "COP",
+          })
+            .format(split.reduce((prev, item) => prev + item.price, 0))
+            .replace(",00", "")}
+        </Text>
+        {split.map((spl) => (
+          <Text className="text-[10rem] font-extrabold text-right">
+            {new Intl.NumberFormat("es-CO", {
+              style: "currency",
+              currency: "COP",
+            })
+              .format(spl.price)
+              .replace(",00", "")}
+          </Text>
+        ))}
+      </View>
     </View>
   ));
+  console.log(splits);
 
   const renderBasketItems = groupedItemsInBasket?.map((item) => (
     <View key={uuid.v4()}>
