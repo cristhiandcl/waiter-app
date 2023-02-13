85659297206b870934800e5eeee5e510ddc24fc7
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-02-13 11:28:57 -0500

    update

--- App.js
+++ App.js
@@ -9,50 +9,53 @@ import SplitAccountScreen from "./screens/SplitAccountScreen";
 import OrdersScreen from "./screens/OrdersScreen";
 import ModifyOrderScreen from "./screens/ModifyOrderScreen";
 import LoginScreen from "./screens/LoginScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Login"
-            component={LoginScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{ headerShown: false, gestureEnabled: false }}
-          />
-          <Stack.Screen
-            name="Restaurant"
-            component={RestaurantScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Basket"
-            component={BasketScreen}
-            options={{ headerShown: false, presentation: "modal" }}
-          />
-          <Stack.Screen
-            name="SplitAccount"
-            component={SplitAccountScreen}
-            options={{ headerShown: false, presentation: "modal" }}
-          />
-          <Stack.Screen
-            name="Orders"
-            component={OrdersScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="ModifyOrder"
-            component={ModifyOrderScreen}
-            options={{ headerShown: false, presentation: "fullScreenModal" }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{ headerShown: false, gestureEnabled: false }}
+            />
+            <Stack.Screen
+              name="Restaurant"
+              component={RestaurantScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Basket"
+              component={BasketScreen}
+              options={{ headerShown: false, presentation: "modal" }}
+            />
+            <Stack.Screen
+              name="SplitAccount"
+              component={SplitAccountScreen}
+              options={{ headerShown: false, presentation: "modal" }}
+            />
+            <Stack.Screen
+              name="Orders"
+              component={OrdersScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="ModifyOrder"
+              component={ModifyOrderScreen}
+              options={{ headerShown: false, presentation: "fullScreenModal" }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    </SafeAreaProvider>
   );
 }
