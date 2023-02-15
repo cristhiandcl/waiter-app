b22cafaa900d2fbc57b4328fcab05138c4a0a76c
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-02-15 15:14:15 -0500

    update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,14 +6,27 @@ import { useEffect } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useDispatch, useSelector } from "react-redux";
 import { getRestaurants, setRestaurants } from "../features/restaurantsSlice";
+import app from "../firebaseConfig";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { useState } from "react";
+
+const db = getFirestore(app);
 
 const HomeScreen = () => {
   const insets = useSafeAreaInsets();
   const navigation = useNavigation();
-  const restaurants = useSelector(getRestaurants);
+  const restaurant = useSelector(getRestaurants)[0];
   const dispatch = useDispatch();
+  const user = getAuth(app).currentUser;
+  const [restaurantName, setRestaurantName] = useState("");
 
   useEffect(() => {
+    (async () => {
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      setRestaurantName(docSnap.data().restaurantName);
+    })();
     client
       .fetch(
         `*[_type == "restaurant"] {_id, description, name, rating, image,
@@ -22,44 +35,61 @@ const HomeScreen = () => {
       .then((data) =>
         dispatch(
           setRestaurants(
-            data.map((restaurant) => ({
-              ...restaurant,
-              id: restaurant._id,
-              dishes: restaurant.dishes.map((dish) => ({
-                ...dish,
-                id: dish._id,
-              })),
-            }))
+            data
+              .map((restaurant) => ({
+                ...restaurant,
+                id: restaurant._id,
+                dishes: restaurant.dishes.map((dish) => ({
+                  ...dish,
+                  id: dish._id,
+                })),
+              }))
+              .filter(
+                (restaurant) => restaurant.name.toLowerCase() === restaurantName
+              )
           )
         )
       );
-  }, []);
+  }, [restaurantName]);
 
+  console.log(restaurantName, restaurant);
   const restaurantTriggered = (id) => {
     navigation.navigate("Restaurant", { id });
   };
 
-  const renderRestaurants = restaurants?.map((restaurant) => (
-    <TouchableOpacity
-      onPress={() => restaurantTriggered(restaurant.id)}
-      key={restaurant.id}
-    >
-      <Image
-        source={{ uri: urlFor(restaurant.image).url() }}
-        className="h-60 w-60 rounded-lg"
-        style={{ resizeMode: "stretch" }}
-      />
-    </TouchableOpacity>
-  ));
+  // const renderRestaurants = restaurant?.map((restaurant) => (
+  //   <TouchableOpacity
+  //     onPress={() => restaurantTriggered(restaurant.id)}
+  //     key={restaurant.id}
+  //   >
+  //     <Image
+  //       source={{ uri: urlFor(restaurant.image).url() }}
+  //       className="h-60 w-60 rounded-lg"
+  //       style={{ resizeMode: "stretch" }}
+  //     />
+  //   </TouchableOpacity>
+  // ));
 
   return (
     <View
-      className="items-center"
+      className="items-center flex-1 justify-center"
       style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}
     >
-      <ScrollView className="space-y-6" showsVerticalScrollIndicator={false}>
+      {/* <ScrollView className="space-y-6" showsVerticalScrollIndicator={false}>
         {renderRestaurants}
-      </ScrollView>
+      </ScrollView> */}
+      {restaurant && (
+        <TouchableOpacity
+          onPress={() => restaurantTriggered(restaurant.id)}
+          key={restaurant.id}
+        >
+          <Image
+            source={{ uri: urlFor(restaurant.image).url() }}
+            className="h-60 w-60 rounded-lg"
+            style={{ resizeMode: "stretch" }}
+          />
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
