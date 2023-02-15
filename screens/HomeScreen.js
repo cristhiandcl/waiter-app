7cbc5e0acf4ab41d45b97956b24449ccfd201fe2
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2023-02-15 12:43:11 -0500

    update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,42 +2,57 @@ import { View, Image, ScrollView, TouchableOpacity } from "react-native";
 import React from "react";
 import { restaurants } from "../restaurants";
 import { useNavigation } from "@react-navigation/core";
-import { client } from "../sanity";
+import { client, urlFor } from "../sanity";
 import { useEffect } from "react";
+import { useState } from "react";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [restaurantes, setRestaurantes] = useState([]);
 
   useEffect(() => {
     client
       .fetch(
-        `*[_type == "restaurant"] {_id, description, name, rating,
+        `*[_type == "restaurant"] {_id, description, name, rating, image,
           dishes[]->{description, name, image, price, _id}}`
       )
-      .then((data) => console.log(data));
-
-    console.log("Rendered");
+      .then((data) => setRestaurantes(data));
   }, []);
-
+  console.log(restaurantes);
   const restaurantTriggered = (id) => {
     navigation.navigate("Restaurant", { id });
   };
-  const renderRestaurants = restaurants.map((restaurant) => (
+  const renderRestaurants = restaurantes.map((restaurant) => (
     <TouchableOpacity
-      onPress={() => restaurantTriggered(restaurant.id)}
-      key={restaurant.id}
+      onPress={() => restaurantTriggered(restaurant._id)}
+      key={restaurant._id}
     >
       <Image
-        source={restaurant.image}
+        source={{ uri: urlFor(restaurant.image).url() }}
         className="h-60 w-60 rounded-lg"
         style={{ resizeMode: "stretch" }}
       />
     </TouchableOpacity>
   ));
 
+  // const renderRestaurants = restaurants.map((restaurant) => (
+  //   <TouchableOpacity
+  //     onPress={() => restaurantTriggered(restaurant.id)}
+  //     key={restaurant.id}
+  //   >
+  //     <Image
+  //       source={restaurant.image}
+  //       className="h-60 w-60 rounded-lg"
+  //       style={{ resizeMode: "stretch" }}
+  //     />
+  //   </TouchableOpacity>
+  // ));
+
   return (
     <View className="mt-12 items-center">
-      <ScrollView className="space-y-6">{renderRestaurants}</ScrollView>
+      <ScrollView className="space-y-6" showsVerticalScrollIndicator={false}>
+        {renderRestaurants}
+      </ScrollView>
     </View>
   );
 };
